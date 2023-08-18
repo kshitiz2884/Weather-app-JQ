13b6ee296aa5fb2267b892755b5c3044f1c0dd19
kshitiz2884 <kshitiz.sri@gmail.com>
2023-08-18 11:45:49 +0530

    Formatted

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-$(document).ready(function() {
-    const apiKey = '0bab7dd1bacc418689b143833220304'; // Replace with your actual API key
-    const baseUrl = 'https://api.weatherapi.com/v1/';
+$(document).ready(function () {
+  const apiKey = "0bab7dd1bacc418689b143833220304"; // Replace with your actual API key
+  const baseUrl = "https://api.weatherapi.com/v1/";
 
-    $('#getWeatherBtn').click(function() {
-        const city = $('#cityInput').val();
-        if (city !== '') {
-            // const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`;
-            const url=`${baseUrl}current.json?key=${apiKey}&q=${city}`;
-            $.ajax({
-                url: url,
-                method: 'GET',
-                success: function(data) {
-                    console.log(data);
-                    displayWeather(data);
-                },
-                error: function() {
-                    $('#weatherInfo').html('<p>Error fetching weather data.</p>');
-                }
-            });
-        }
-    });
+  $("#getWeatherBtn").click(function () {
+    const city = $("#cityInput").val();
+    if (city !== "") {
+      // const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`;
+      const url = `${baseUrl}current.json?key=${apiKey}&q=${city}`;
+      $.ajax({
+        url: url,
+        method: "GET",
+        success: function (data) {
+          console.log(data);
+          displayWeather(data);
+        },
+        error: function () {
+          $("#weatherInfo").html("<p>Error fetching weather data.</p>");
+        },
+      });
+    }
+  });
 
-    function displayWeather(data) {
-        const cityName = data.location.name;
-        const region=data.location.region
-        const temperature = data.current.temp_c;
-        const humidity = data.current.humidity;
-        const pre= data.current.condition.text;
-        const visibility = data.current.vis_km;
+  function displayWeather(data) {
+    const cityName = data.location.name;
+    const region = data.location.region;
+    const temperature = data.current.temp_c;
+    const humidity = data.current.humidity;
+    const pre = data.current.condition.text;
+    const visibility = data.current.vis_km;
 
-        const weatherHtml = `
+    const weatherHtml = `
             <h2>${cityName}</h2>
             <h3>${region}</h3>
             <p>Temp: ${temperature}°C</p>
@@ -38,6 +38,6 @@ $(document).ready(function() {
             <p>Current Weather: ${pre}</p>
         `;
 
-        $('#weatherInfo').html(weatherHtml);
-    }
+    $("#weatherInfo").html(weatherHtml);
+  }
 });
